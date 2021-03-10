3a52348d7b90a81303e15f66218864e7f5920099
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2021-03-11 00:41:44 +0100

    styled API

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,40 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const API_URL = "https://api.punkapi.com/v2/beers";
+const container = document.querySelector(".container");
+
+const render = (data) => {
+  if (!data.length) return;
+  const fragment = document.createDocumentFragment();
+  data.forEach(({ name, tagline, description, image_url: imageURL }) => {
+    const div = document.createElement("div");
+    div.classList.add("beer");
+    div.innerHTML = `
+    <img class="beer__image" src="${imageURL}"/>
+    <div class="beer__content">
+        <h2 class="beer__title">${name}</h2>
+            <p class="beer__tagline">${tagline}</p>
+             <p class="beer__description">${description}</p>
+      </div>
+   `;
+    fragment.appendChild(div);
+  });
+  container.appendChild(fragment);
+};
+
+const success = (data) => {
+  const beers = JSON.parse(data.target.responseText);
+  render(beers);
+  //   console.log(beers[1].name);
+};
+
+const error = (err) => {
+  console.log(err);
+};
+
+const req = new XMLHttpRequest();
+req.onload = success;
+req.onerror = error;
+req.open("GET", API_URL);
+req.send();
